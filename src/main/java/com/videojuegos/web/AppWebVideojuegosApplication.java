d7aa2622456d86973a3b07e7198a6aae61a1a03b
web videojuegos <u17303183@utp.edu.pe>
2025-10-24 20:32:16 -0500

    first commit

---
+++ src/main/java/com/videojuegos/web/AppWebVideojuegosApplication.java
@@ -0,0 +1,13 @@
+package com.videojuegos.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebVideojuegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebVideojuegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videojuegos/web/AppWebVideojuegosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videojuegos.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebVideojuegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
